1f62ab9cd967d0780b1b88d673833fa2f9f7783a
rajmoham <rajmoham10@gmail.com>
2023-11-24 13:48:23 +0000

    migrations resolution

--- tasks/migrations/0010_merge_20231117_1410.py
+++ tasks/migrations/0010_merge_20231117_1410.py
@@ -7,7 +7,6 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('tasks', '0009_merge_20231117_1339'),
-        ('tasks', '0009_merge_20231117_1340'),
     ]
 
     operations = [
