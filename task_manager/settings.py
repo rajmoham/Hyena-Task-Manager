9fd80c12ea5a99f38f6386bc8224b1d3244e49a2
rajmoham <rajmoham10@gmail.com>
2023-12-09 15:56:12 +0000

    notifications on dashboard ui

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -65,7 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'tasks.views.unseen_notifications'
+                'tasks.views.unseen_notifications',
             ],
         },
     },
